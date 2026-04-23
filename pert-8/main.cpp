0d2622bb69b8529552c68a4a4f048a5f83408c11
Raul iqbal <89615372+Rauliqbal@users.noreply.github.com>
2026-04-23 21:26:03 +0700

    pertemuan 8 : Control Statement IF

--- pert-8/main.cpp
+++ pert-8/main.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int A,B,C;
+
+    cout << "Masukkan nilai A : ";
+    cin >> A;
+
+    cout << "Masukkan nilai B : ";
+    cin >> B;
+
+    cout << "Masukkan nilai C : ";
+    cin >> C;
+
+    if(A < B) {
+        if(B < C) {
+            cout << "\nurutan nilai terkecil: " << A << " " << B << " " << C << " ";
+        } else {
+            if(A < C) {
+                cout << "\nurutan nilai terkecil: " << A << " " << C << " " << B << " ";
+            } else {
+                cout << "\nurutan nilai terkecil: " << C << " " << A << " " << B << " ";
+            }
+        }
+    } else {
+        if(A < C) {
+            cout << "\nurutan nilai terkecil: " << B << " " << A << " " << C << " ";
+        } else {
+            if(B < C) {
+                cout << "\nurutan nilai terkecil: " << B << " " << C << " " << A << " ";
+            } else {
+                cout << "\nurutan nilai terkecil: " << C << " " << B << " " << A << " ";
+            }
+        }
+    }
+
     return 0;
 }
